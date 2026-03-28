3d79605e2fece50efb65d20fe61568814fad650c
yashwyneric <yashwyneric@gmail.com>
2026-03-28 23:55:53 +0530

    FREE DEPLOY: Launching MVP without trials

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "886999476" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3727165086" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
